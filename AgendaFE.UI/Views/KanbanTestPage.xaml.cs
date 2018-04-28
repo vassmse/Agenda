a4ca2af2f671b128f65d6b330b712e99fe43f694
Emese Vass <vassmse@gmail.com>
2018-04-28 22:26:03 +0200

    ..

--- AgendaFE.UI/Views/KanbanTestPage.xaml.cs
+++ AgendaFE.UI/Views/KanbanTestPage.xaml.cs
@@ -3,14 +3,17 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel.DataTransfer;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Storage;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
@@ -25,6 +28,31 @@ namespace AgendaFE.UI.Views
         public KanbanTestPage()
         {
             this.InitializeComponent();
+        }
+
+        private async void Grid_Drop(object sender, DragEventArgs e)
+        {
+            if (e.DataView.Contains(StandardDataFormats.StorageItems))
+            {
+                var items = await e.DataView.GetStorageItemsAsync();
+                if (items.Count > 0)
+                {
+                    var storageFile = items[0] as StorageFile;
+                    var bitmapImage = new BitmapImage();
+                    bitmapImage.SetSource(await storageFile.OpenAsync(FileAccessMode.Read));
+                    // Set the image on the main page to the dropped image
+                    //Image.Source = bitmapImage;
+                }
+            }
+        }
+
+        private void Grid_DragOver(object sender, DragEventArgs e)
+        {
+            e.AcceptedOperation = DataPackageOperation.Move;
+            e.DragUIOverride.Caption = "You can drop it here.";
+            e.DragUIOverride.IsGlyphVisible = true;
+            e.DragUIOverride.IsContentVisible = true;
+            e.DragUIOverride.IsCaptionVisible = true;
         }
     }
 }
